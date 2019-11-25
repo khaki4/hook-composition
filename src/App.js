7177a613edce86535655723552e1bb4371e46c44
pi3rrot <4dakto@gmail.com>
2019-11-25 17:50:47 +0900

    input value

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useLocalStorage } from "./hooks";
 
 function App() {
+  const [value, setValue] = useLocalStorage("inputValue", "");
+  const onChange = React.useCallback(e => {
+    setValue(e.currentTarget.value);
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="test">
+        <input value={value} onChange={onChange} />
+      </div>
     </div>
   );
 }
+
+function Message(props) {}
 
 export default App;
