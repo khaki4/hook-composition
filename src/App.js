ee0e1f4c963b93756455974aae89c97bf468962f
pi3rrot <4dakto@gmail.com>
2019-11-25 18:04:39 +0900

    add hook composition

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { useLocalStorage } from "./hooks";
 
+function useUndo([state, setState]) {
+  const historyRef = useRef([state]);
+  const [index, setIndex] = useState(0);
+
+  function undo() {
+    setIndex(index - 1);
+  }
+  function redo() {
+    setIndex(index + 1);
+  }
+  function newSetState(nextState) {
+    const nextIndex = index + 1;
+    historyRef.current[nextIndex] = nextState;
+    setIndex(nextIndex);
+    setState(nextState);
+  }
+
+  return [historyRef.current[index], newSetState, undo, redo];
+}
+
 function App() {
-  const [value, setValue] = useLocalStorage("inputValue", "");
+  const [value, setValue, undo, redo] = useUndo(useLocalStorage("inputValue", ""));
   const onChange = React.useCallback(e => {
     setValue(e.currentTarget.value);
   });
   return (
     <div className="App">
       <div className="test">
+        <button onClick={undo}>undo</button>
         <input value={value} onChange={onChange} />
+        <button onClick={redo}>redo</button>
       </div>
     </div>
   );
 }
-
-function Message(props) {}
 
 export default App;
